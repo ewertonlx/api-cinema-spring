a84f8038e3f7fc9e8d1405b51761c5d0fcf797a0
ewertonlx <ewerton123ofc@gmail.com>
2025-05-16 20:47:38 -0300

    refact: implementing FeedBackDTO

--- demo/src/main/java/com/cinemaapi/demo/services/FeedBackService.java
+++ demo/src/main/java/com/cinemaapi/demo/services/FeedBackService.java
@@ -3,7 +3,10 @@ package com.cinemaapi.demo.services;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+
+import com.cinemaapi.demo.dto.FeedBackDTO;
 import com.cinemaapi.demo.entity.Feedback;
+import com.cinemaapi.demo.exception.NotFoundException;
 import com.cinemaapi.demo.repository.FeedBackRepository;
 import com.cinemaapi.demo.repository.MovieRepository;
 
@@ -11,54 +14,34 @@ import com.cinemaapi.demo.repository.MovieRepository;
 public class FeedBackService {
     private final FeedBackRepository feedbackRepository;
     private final MovieRepository movieRepository;
-    private final MovieService movieService;
 
-    public FeedBackService(FeedBackRepository feedbackRepository, MovieRepository movieRepository, MovieService movieService) {
-        this.movieService = movieService;
+    public FeedBackService(FeedBackRepository feedbackRepository, MovieRepository movieRepository) {
         this.feedbackRepository = feedbackRepository;
         this.movieRepository = movieRepository;
     }
 
-    public Feedback addFeedbackToMovie(int movieId, Feedback feedback) {
-        var movie = movieRepository.findById(movieId).orElse(null);
-        if (movie == null) {
-            throw new RuntimeException("Filme não encontrado!");
-        }
-        if(feedback.getUsername() == null || feedback.getUsername().isEmpty()
-        || feedback.getComment() == null || feedback.getComment().isEmpty()
-        ){
-            throw new RuntimeException("Preencha todos os campos!");
-        }
-        if(feedback.getRating() < 0 || feedback.getRating() > 5){
-            throw new RuntimeException("Avaliação deve ser entre 0 e 5!");
-        }
-
-        feedback.setMovie(movie);
-        return feedbackRepository.save(feedback);
+    public FeedBackDTO addFeedbackToMovie(int movieId, FeedBackDTO feedback) {
+        var movie = movieRepository.findById(movieId).orElseThrow(() -> new NotFoundException("Filme não encontrado!"));
+        Feedback feedbackEntity = new Feedback(feedback.username(), feedback.comment(), feedback.rating(), movie);
+        Feedback savedFeedback = feedbackRepository.save(feedbackEntity);
+        return new FeedBackDTO(savedFeedback.getUsername(), savedFeedback.getComment(), savedFeedback.getRating());
     }
 
-    public Feedback updateFeedback(int id, Feedback feedback) {
-        var existingFeedback = feedbackRepository.findById(id).orElse(null);
-        if (existingFeedback == null) {
-            throw new RuntimeException("Feedback não encontrado!");
-        }
-        if(feedback.getComment() == null || feedback.getComment().isEmpty()){
-            throw new RuntimeException("Preencha todos os campos!");
-        }
-        if(feedback.getRating() < 0 || feedback.getRating() > 5){
-            throw new RuntimeException("Avaliação deve ser entre 0 e 5!");
-        }
+    public FeedBackDTO updateFeedback(int id, FeedBackDTO feedback) {
+        var existingFeedback = feedbackRepository.findById(id).orElseThrow(() -> new NotFoundException("Feedback não encontrado!"));
 
         existingFeedback.setComment(feedback.getComment());
         existingFeedback.setRating(feedback.getRating());
-        return feedbackRepository.save(existingFeedback);
+        Feedback updatedFeedback = feedbackRepository.save(existingFeedback);
+        return new FeedBackDTO(updatedFeedback.getUsername(), updatedFeedback.getComment(), updatedFeedback.getRating());
     }
 
-    public List<Feedback> getMovieFeedbacks(int id) {
-        var movie = movieService.getMovieById(id);
-        if( movie == null){
-            throw new RuntimeException("Filme não encontrado!");
-        }
-        return movie.getFeedbacks();
+    public List<FeedBackDTO> getMovieFeedbacks(int id) {
+        var movie = movieRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("Filme não encontrado!"));
+
+        return movie.getFeedbacks().stream()
+            .map(f -> new FeedBackDTO(f.getUsername(), f.getComment(), f.getRating()))
+            .toList();
     }
 }
